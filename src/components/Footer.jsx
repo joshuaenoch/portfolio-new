e2c7f5a2c3dafd2cb8bcaaddd40ae92652189c29
joshuaenoch <joshuaechow@gmail.com>
2025-01-07 12:00:36 -0800

    project read more and descriptions implemented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="footer">
-      <a className="footer-item" target="_blank" href="https://www.linkedin.com/in/joshuaechow/">
+      <a className="footer-item" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/joshuaechow/">
       Linkedin •&nbsp;</a>
-      <a className="footer-item" target="_blank" href="https://github.com/joshuaenoch">
+      <a className="footer-item" target="_blank" rel="noopener noreferrer" href="https://github.com/joshuaenoch">
       Github •&nbsp;</a>
-      <a className="footer-item" target="_blank" href="https://uw.joinhandshake.com/profiles/32104571">
+      <a className="footer-item" target="_blank" rel="noopener noreferrer" href="https://uw.joinhandshake.com/profiles/32104571">
       Handshake</a>
       {/*<img src="/linkedin.jpg"></img>
       <img src="/github.png"></img>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,32 +1,51 @@
-import React from 'react'
-import data from '../data.json'
+import React, { useState } from 'react';
+import data from '../data.json';
 
 export default function Projects() {
   let projects = data.projects;
 
   return (
     <div className="project-list">
-      {projects.map((project) => (
-        <div className="project-card">
-          <img className="project-image" src={project.image} />
-          <h3 className="project-name">{project.name}</h3>
-          <div className="project-buttons">
-            <div className="project-link">Read more</div>
-            {project.link == "" ? <></> :
-            <>
-              <div className="vr"></div>
-              {project.link[0] == "Link" ?
-                            <a className="project-link" href={project.link[1]} target="_blank">
-                              Try it out</a>:
-                            <a className="project-link" href={project.link[1]} target="_blank">
-                              See repository</a>
-              }
-            </>
-             }
+      {projects.map((project, index) => {
+        const [isOpened, setIsOpened] = useState(false);
+
+        // Toggle the visibility of the description
+        const readMore = () => {
+          setIsOpened(!isOpened);
+        };
+
+        return (
+          <div key={index} className="project-card">
+            <img className="project-image" src={project.image} alt={project.name} />
+            <h3 className="project-name">{project.name}</h3>
+            <div className="project-buttons">
+              <div className="project-link" onClick={readMore}>
+                {isOpened ? "Close" : "Read more"}
+              </div>
+              {project.link === "" ? null : (
+                <>
+                  <div className="vr"></div>
+                  {project.link[0] === "Link" ? (
+                    <a className="project-link" href={project.link[1]} target="_blank" rel="noopener noreferrer">
+                      Try it out
+                    </a>
+                  ) : (
+                    <a className="project-link" href={project.link[1]} target="_blank" rel="noopener noreferrer">
+                      See repository
+                    </a>
+                  )}
+                </>
+              )}
+            </div>
+            {isOpened && (
+              <div className="project-description">
+                <div>{project.description}</div>
+                <div>Made with: {project.technologies.join(", ")}</div>
+              </div>
+            )}
           </div>
-        </div>
-      ))
-      }
+        );
+      })}
     </div>
-  )
+  );
 }
